e40993b5582b620f5cfa581fc4c1397417eb1479
gaoooon <gaon9749@gmail.com>
2023-05-29 14:46:34 +0900

    Update

---
+++ coupang/script.js
@@ -0,0 +1,15 @@
+const checkBox = document.querySelectorAll(".checkbox");
+
+function handleClickCheckBox(event) {
+  const target = event.target;
+  if (target.classList.contains("img")) {
+    target.classList.remove("img");
+    target.classList.add("imgChange");
+  } else if (target.classList.contains("imgChange")) {
+    target.classList.remove("imgChange");
+    target.classList.add("img");
+  }
+}
+for (i = 0; i < 11; i++) {
+  checkBox[i].addEventListener("click", handleClickCheckBox);
+}
